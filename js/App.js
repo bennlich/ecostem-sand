947934e8e2ec3f8d65f252068f179f70b724086b
Marius Nita <m@mariusnita.com>
2014-05-08 20:16:10 -0600

    add new deps, skeleton map app using react and st-api

--- js/App.js
+++ js/App.js
@@ -1,13 +1,21 @@
 "use strict";
 
+import '../st-api/Util/StringUtil';
 import {Leaflet} from '../st-api/Leaflet';
+import {ModelPool} from '../st-api/ModelingCore/ModelPool';
+import {Animator} from '../st-api/ModelingCore/Animator';
+import {TransferFunctions} from '../st-api/ModelingParams/TransferFunctions';
+import {RemoteBBoxSampler} from '../st-api/RemoteBBoxSampler';
 
 var D = React.DOM;
 
+var tfMockUI = React.createClass({
+    render: () => D.div({id: 'transfer-function-svg'}),
+    componentDidMount: () => TransferFunctions.init()
+});
+
 var leafletUI = React.createClass({
-    render: () => {
-        return D.div({id: 'leafletMap'});
-    },
+    render: () => D.div({id: 'leafletMap'}),
     componentDidMount: () => {
         var south = 33.357555,
             west = -105.890007,
@@ -19,9 +27,33 @@ var leafletUI = React.createClass({
             new L.LatLng(north, east)
         );
 
-        this.map = new Leaflet('leafletMap', bounds);
-        this.map.setHomeView();
-        this.map.addLayers();
+        var map = window.map = new Leaflet('leafletMap', bounds);
+
+        map.setHomeView();
+        map.addLayers();
+
+        var modelPool = window.modelPool = new ModelPool(map);
+
+        var elevModel = window.elevModel = modelPool.getModel('Elevation');
+        var waterModel = window.waterModel = modelPool.getModel('Water Flow');
+
+        map.toggleLayer({
+            on: false,
+            leafletLayer: waterModel.renderer.makeLayer({zIndex:20, opacity:0.85})
+        });
+
+        var animator = window.animator = new Animator(modelPool);
+
+        var sampler = new RemoteBBoxSampler(
+            document.createElement('canvas'),
+            'http://node.redfish.com/cgi-bin/elevation.py?bbox={s},{w},{n},{e}&res={width},{height}'
+        );
+
+        sampler.loadRemoteData(elevModel.dataModel.geometry, () => {
+            elevModel.dataModel.loadElevation(sampler);
+            waterModel.dataModel.sampleElevation();
+            animator.start();
+        });
     }
 });
 
@@ -34,6 +66,7 @@ var menuUI = React.createClass({
 var mainUI = React.createClass({
     render: () => {
         return D.div({}, [
+            tfMockUI(),
             leafletUI(),
             menuUI()
         ]);
