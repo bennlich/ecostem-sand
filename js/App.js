812db29f525810be7353775f27f7ad6e8dacb6e9
Marius Nita <m@mariusnita.com>
2014-05-09 15:27:43 -0600

    start/stop buttons

--- js/App.js
+++ js/App.js
@@ -6,17 +6,11 @@ import {ModelPool} from '../st-api/ModelingCore/ModelPool';
 import {Animator} from '../st-api/ModelingCore/Animator';
 import {TransferFunctions} from '../st-api/ModelingParams/TransferFunctions';
 import {RemoteBBoxSampler} from '../st-api/RemoteBBoxSampler';
+import {Evented} from '../st-api/ModelingCore/Evented';
 
-var D = React.DOM;
-
-var tfMockUI = React.createClass({
-    render: () => D.div({id: 'transfer-function-svg'}),
-    componentDidMount: () => TransferFunctions.init()
-});
-
-var leafletUI = React.createClass({
-    render: () => D.div({id: 'leafletMap'}),
-    componentDidMount: () => {
+class Application extends Evented {
+    constructor() {}
+    initialize(mapDivId) {
         var south = 33.357555,
             west = -105.890007,
             north = 33.525149,
@@ -27,22 +21,22 @@ var leafletUI = React.createClass({
             new L.LatLng(north, east)
         );
 
-        var map = window.map = new Leaflet('leafletMap', bounds);
+        this.map = window.map = new Leaflet(mapDivId, bounds);
 
-        map.setHomeView();
-        map.addLayers();
+        this.map.setHomeView();
+        this.map.addLayers();
 
-        var modelPool = window.modelPool = new ModelPool(map);
+        this.modelPool = new ModelPool(this.map);
 
-        var elevModel = window.elevModel = modelPool.getModel('Elevation');
-        var waterModel = window.waterModel = modelPool.getModel('Water Flow');
+        var elevModel = this.modelPool.getModel('Elevation');
+        var waterModel = this.modelPool.getModel('Water Flow');
 
-        map.toggleLayer({
+        this.map.toggleLayer({
             on: false,
             leafletLayer: waterModel.renderer.makeLayer({zIndex:20, opacity:0.85})
         });
 
-        var animator = window.animator = new Animator(modelPool);
+        this.animator = new Animator(this.modelPool);
 
         var sampler = new RemoteBBoxSampler(
             document.createElement('canvas'),
@@ -52,19 +46,63 @@ var leafletUI = React.createClass({
         sampler.loadRemoteData(elevModel.dataModel.geometry, () => {
             elevModel.dataModel.loadElevation(sampler);
             waterModel.dataModel.sampleElevation();
-            animator.start();
         });
+    }
+}
+
+var D = React.DOM;
+
+var tfMockUI = React.createClass({
+    render: function() {
+        return D.div({id: 'transfer-function-svg'});
+    },
+    componentDidMount: function() {
+        TransferFunctions.init();
+    }
+});
+
+var leafletUI = React.createClass({
+    render: function() {
+        return D.div({id: 'leafletMap'});
+    },
+    componentDidMount: function() {
+        app.initialize('leafletMap');
     }
 });
 
 var menuUI = React.createClass({
-    render: () => {
-        return D.div({className: 'menu'}, 'hello');
+    getInitialState: function() {
+        return {started: false};
+    },
+    updateState: function() {
+        this.setState({started: app.animator.isRunning});
+    },
+    handleStartClick: function() {
+        app.animator.start();
+        this.updateState();
+    },
+    handleStopClick: function() {
+        app.animator.stop();
+        this.updateState();
+    },
+    handleReset: function() {
+        app.animator.reset();
+        this.updateState();
+    },
+    render: function() {
+        var startButton = D.button({onClick: this.handleStartClick}, 'Start');
+        var stopButton = D.button({onClick: this.handleStopClick}, 'Stop');
+
+        return D.div({className: 'menu'}, [
+            this.state.started ? stopButton : startButton,
+            D.button({onClick: this.handleReset}, 'Reset')
+        ]);
     }
 });
 
 var mainUI = React.createClass({
-    render: () => {
+    render: function() {
+        console.log(this.props.app);
         return D.div({}, [
             tfMockUI(),
             leafletUI(),
@@ -73,4 +111,5 @@ var mainUI = React.createClass({
     }
 });
 
+var app = new Application();
 React.renderComponent(mainUI(), $('.app')[0]);
