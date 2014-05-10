079ca40cc69bd9d0a4af5fb46d8274acd60a252a
Marius Nita <m@mariusnita.com>
2014-05-09 19:39:24 -0600

    add push script that adds st-api to gh-pages

---
+++ gh-pages-push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+repo=ecostem-sand
+
+mkdir gh_pages_tmp && \
+pushd gh_pages_tmp && \
+  git clone git@github.com:mariusnita/$repo.git && \
+  pushd $repo && \
+    (git push origin --delete gh-pages || true ) && \
+    git branch gh-pages && \
+    git checkout gh-pages && \
+    git pull origin master && \
+    git clone git@github.com:mariusnita/st-api.git && \
+    rm -rf st-api/.git && \
+    git add st-api && \
+    (git commit -a -m 'update' || true) && \
+    (git push origin gh-pages || true) && \
+  popd && \
+popd && \
+rm -rf gh_pages_tmp
--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git checkout master && \
-git push && \
-git checkout gh-pages && \
-git pull origin master && \
-git push && \
-git checkout master
