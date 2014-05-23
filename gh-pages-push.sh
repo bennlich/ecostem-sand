107a9bc32b6073b9870378e986d7225df6f65ee2
Marius Nita <m@mariusnita.com>
2014-05-23 14:10:59 -0600

    update gh-pages push script for new simtable target

--- gh-pages-push.sh
+++ gh-pages-push.sh
@@ -4,13 +4,13 @@ repo=ecostem-sand
 
 mkdir gh_pages_tmp && \
 pushd gh_pages_tmp && \
-  git clone git@github.com:mariusnita/$repo.git && \
+  git clone git@github.com:simtable/$repo.git && \
   pushd $repo && \
     (git push origin --delete gh-pages || true ) && \
     git branch gh-pages && \
     git checkout gh-pages && \
     git pull origin master && \
-    git clone git@github.com:mariusnita/st-api.git && \
+    git clone git@github.com:simtable/st-api.git && \
     rm -rf st-api/.git && \
     git add st-api && \
     (git commit -a -m 'update' || true) && \
