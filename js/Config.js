f446bc2f918742ae7de0f99b8bac93027d94e0a2
Marius Nita <m@mariusnita.com>
2014-06-06 19:13:12 -0600

    move the camera url into Config

--- js/Config.js
+++ js/Config.js
@@ -3,7 +3,8 @@ export var Config = {
     /* All the data will be 2^vertFrames x 2^horizFrames in size. This is in tight
        inter-dependence with the fact that StripeScan will project vertFrames
        vertical-striped frames and horizFrames horizontal-striped frames. */
-
     vertFrames : 7,
-    horizFrames : 7
+    horizFrames : 7,
+    /* Any HTTP server serving a JPG of the scene will do */
+    cameraServer: "http://192.168.1.109:8080/shot.jpg"
 };
--- js/StripeScan.js
+++ js/StripeScan.js
@@ -7,7 +7,7 @@ export class StripeScan {
     constructor() {
         window.ss = this;
 
-        this.server = "http://192.168.1.109:8080/shot.jpg";
+        this.server = Config.cameraServer;
         console.log(this.server);
         this.imageLoader = new ImageLoader();
 
